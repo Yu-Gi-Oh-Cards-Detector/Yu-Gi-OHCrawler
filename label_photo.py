a9fd57d335286544a1b5ea0ff07e45863cb27dc1
XinZhi <hellen6654@gmail.com>
2021-05-04 16:04:06 +0800

    fix bndbox

--- label_photo.py
+++ label_photo.py
@@ -10,9 +10,9 @@ XML_VERSION_STRING = '<?xml version="1.0" ?>\n'
 dir_list = os.listdir(STATIC)
 for d in dir_list:
     root = ET.Element("annotation")
-    SubElement(root, "folder").text = STATIC
+    SubElement(root, "folder").text = STATIC+'label'
     SubElement(root, "filename").text = f"{d}.jpg"
-    SubElement(root, "path").text = f"{STATIC}/{d}/{d}.jpg"
+    SubElement(root, "path").text = f"{STATIC}/label/{d}.jpg"
     SubElement(SubElement(root, "source"), "database").text = "Yu-Gi-Oh"
 
     # Size
@@ -32,14 +32,14 @@ for d in dir_list:
 
     # bndbox
     bndbox = SubElement(obj, "bndbox")
-    SubElement(bndbox, "xmin").text = "49"
-    SubElement(bndbox, "ymin").text = "273"
-    SubElement(bndbox, "xmax").text = "382"
-    SubElement(bndbox, "ymax").text = "596"
+    SubElement(bndbox, "xmin").text = "44"
+    SubElement(bndbox, "ymin").text = "110"
+    SubElement(bndbox, "xmax").text = "377"
+    SubElement(bndbox, "ymax").text = "433"
 
     tree = ElementTree(root)
     formatted = minidom.parseString(ET.tostring(root)).toprettyxml()
     formatted = formatted.replace(XML_VERSION_STRING, "")
-    with open(f"{STATIC}/{d}/{d}.xml", "w", encoding="utf-8") as f:
+    with open(f"{STATIC}/label/{d}.xml", "w", encoding="utf-8") as f:
         f.write(formatted)
     print(dir_list.index(d)+1,'/',len(dir_list))
